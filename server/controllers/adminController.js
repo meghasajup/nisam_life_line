cc19c3f9cd5247f695f7f2c0fbb1c51c540197c6
Your Megha Saju P <Your meghasaju45@gmail.com>
2025-09-11 19:48:00 +0530

    Remove sub domain

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -19,7 +19,7 @@ export const adminLogin = asyncHandler(async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "None", 
-      domain: ".nisam-life-line.com",
+      //domain: ".nisam-life-line.com",
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
