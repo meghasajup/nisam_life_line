4962640288f6294476aa7dd7169805c707366a4a
Your Megha Saju P <Your meghasaju45@gmail.com>
2025-09-11 19:23:35 +0530

    Remove sub domain

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -19,7 +19,7 @@ export const adminLogin = asyncHandler(async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "None", 
-      domain: ".vercel.app", 
+      //domain: ".vercel.app", 
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
