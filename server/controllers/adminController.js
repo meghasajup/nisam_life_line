f6ea8b04a85d221c0d3f75ba354ac4985aca571f
Your Megha Saju P <Your meghasaju45@gmail.com>
2025-09-11 19:21:56 +0530

    Add sub domain name

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -18,8 +18,8 @@ export const adminLogin = asyncHandler(async (req, res) => {
     res.cookie("AdminToken", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax", 
-      domain: ".vercel.app",
+      sameSite: "None", 
+      domain: ".vercel.app", 
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
