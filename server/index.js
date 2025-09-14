6fdd4562b979cbabb93c22137fb74bbbce13326c
meghasajup <meghasaju45@gmail.com>
2025-09-14 16:30:30 +0530

    AdminToken name change fix

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ import cors from 'cors'
 const app = express()
 app.use(cors({
   origin: [
-    'https://nisam-lifeline-admin.vercel.app',
-    'https://nisam-life-line.vercel.app',
-    //'http://localhost:5173',
-    //'http://localhost:5174'
+    //'https://nisam-lifeline-admin.vercel.app',
+    //'https://nisam-life-line.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:5174'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
--- server/middleware/verifyAdminToke.js
+++ server/middleware/verifyAdminToke.js
@@ -1,17 +1,23 @@
-import jwt from 'jsonwebtoken';
-
+import jwt from "jsonwebtoken";
 
 export const verifyAdminToken = (req, res, next) => {
-  req.cookies.adminToken || req.headers['authorization']?.replace(/^Bearer\s/, '');
+  // assign the token to a variable
+  const token =
+    req.cookies?.AdminToken ||
+    req.headers["authorization"]?.replace(/^Bearer\s/, "");
 
   if (!token) {
-    return res.status(403).json({ success: false, message: 'Access denied. No token provided.' });
+    return res
+      .status(403)
+      .json({ success: false, message: "Access denied. No token provided." });
   }
+
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.admin = verified;
     next();
   } catch (error) {
-    res.status(400).json({ success: false, error: 'Invalid token' });
+    console.error("JWT verification failed:", error.message);
+    return res.status(401).json({ success: false, error: "Invalid or expired token" });
   }
 };
