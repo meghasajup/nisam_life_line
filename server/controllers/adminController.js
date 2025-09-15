7c652946f5977d268fb8254458e4496226cdb6e8
JESWIN100 <josepjjeswin20@gmail.com>
2025-09-15 22:30:34 +0530

    bekbescoolier

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -17,7 +17,6 @@ export const adminLogin = asyncHandler(async (req, res) => {
     // Set cookie with better mobile compatibility
    res.cookie("AdminToken", token, {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production", 
   sameSite: "Lax", 
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   secure:true,
--- server/middleware/verifyAdminToke.js
+++ server/middleware/verifyAdminToke.js
@@ -3,10 +3,10 @@ import jwt from 'jsonwebtoken';
 
 export const verifyAdminToken = (req, res, next) => {
   const token = req.cookies?.AdminToken || req.headers['authorization']?.replace(/^Bearer\s/, '');
-  const Secounftoken = req.cookie?.AdminToken || req.headers['authorization']?.replace(/^Bearer\s/, '');
+ 
 console.log("sdsd",token);
 
-  if (!token || Secounftoken) {
+  if (!token) {
     return res.status(403).json({ success: false, message: 'Access denied. No token provided.' });
   }
   try {
